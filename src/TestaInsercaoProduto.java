a7343db984af38b8f8f75f5b828b6fd8de07d516
WillianFarias <williansousafarias@gmail.com>
2022-06-11 19:22:29 -0300

    refactor estrutura do projeto e classe dao

--- src/TestaInsercaoProduto.java
+++ src/TestaInsercaoProduto.java
@@ -1,32 +1,17 @@
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import dao.ProdutoDAO;
+import model.Produto;
 
 public class TestaInsercaoProduto {
 
 	public static void main(String[] args) throws SQLException {
 
 		Produto comoda = new Produto("Comoda", "Comoda Vertical");
-		
-		try(Connection connection = new ConnectionFactory().recuperaConexao()) {
-			
-			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?,?)";
-			
-			try(PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				pstm.setString(1, comoda.getNome());
-				pstm.setString(1, comoda.getDescricao());
-				
-				pstm.execute();
-				
-				try(ResultSet rst = pstm.getGeneratedKeys()) {
-					while(rst.next()) {
-						comoda.setId(rst.getInt(1));
-					}
-				}
-			}
+
+		try (Connection connection = new ConnectionFactory().recuperaConexao()) {
+			new ProdutoDAO(connection).salvar(comoda);
 		}
-		System.out.println(comoda);
 	}
 }
